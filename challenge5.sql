6ea2232bb84719497c0a8af09866904571aa3587
Timothy Kao <timot1@MacBook-Pro.local>
2021-04-22 17:04:23 -0400

    ammend query for 5

--- challenge5.sql
+++ challenge5.sql
@@ -1,3 +1,8 @@
-SELECT A.Name, B.Title, MIN(B.Publication_Year) AS "First_Pub_Year" FROM Book B
-JOIN Author A ON B.Author = A.id
+CREATE TEMP VIEW FIRST_YEAR AS
+SELECT A.id, MIN(B.Publication_Year) AS Publication_Year FROM Book B, Author A
+WHERE A.id = B.Author
 GROUP BY A.id;
+
+SELECT A.Name, B.Title, F.Publication_Year FROM Book B
+JOIN Author A ON B.Author = A.id
+JOIN FIRST_YEAR F ON F.id = B.Author AND F.Publication_Year = B.Publication_Year;
